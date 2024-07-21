fb7209b5f33787515cc0d52d2dc325cdb2347f08
Poonam Kale <omkarvkale123@gmail.com>
2024-07-21 12:20:25 +0530

    file updated

--- application.py
+++ application.py
@@ -68,4 +68,4 @@ def predict():
     return render_template('index.html',prediction=prediction)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8080,debug=True)
+    app.run(host="0.0.0.0",debug=True)
